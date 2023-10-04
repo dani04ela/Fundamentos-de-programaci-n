1a28075e13ed57233b9eda2c47a5147dcbaf60e9
dani04ela <146204223+dani04ela@users.noreply.github.com>
2023-10-04 10:05:42 -0400

    Add files via upload

---
+++ m .py
@@ -0,0 +1,12 @@
+#este proceso calcula el area de un triangulo
+#defino base y altura
+base: int
+altura: int
+resultado : float
+#los pido al usuario
+base= int(input("n1:"))
+altura= int(input("n2:"))
+#lo multiplico y lo divido entre 2
+resultado = base*altura/2
+#le muestro el resultado al usuario
+print (resultado)
